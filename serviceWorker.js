96279321cd9e0266e2e03b8b717b1c7d4075dbcf
Kay <kay@madpat.de>
2016-07-01 11:56:09 +0200

    new version deploy gh

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 /**
  * Created by ikay on 01.07.16.
  */
-var cacheName = 'notencache58s8383assddSD882adsd';
+var cacheName = 'notenrechnerjk82982932893';
 var filesToCache = [
   '/',
   '/manifest.json',
